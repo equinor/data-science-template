9147ff348b683fd229f3963cede5bb77cbbed8f3
darren-taylor <darren.taylor@woodside.com.au>
2019-05-07 17:46:57 +0800

    Git based versioning
    Changed from setuptools-git-version as used by Woodside to better-setuptools-git-version

--- setup.py
+++ setup.py
@@ -12,9 +12,12 @@ def read(file_name):
 
 setup(
     name="example_datascience_project",
-    # version="0.0.1",
-    version_format='{tag}.{commitcount}+{gitsha}',
-    setup_requires=['setuptools-git-version'],
+    version_config={
+      "version_format": "{tag}.dev{sha}",
+      # version="0.0.1",
+      "starting_version": "0.0.1"
+    },
+    setup_requires=['better-setuptools-git-version'],
     author='Statoil ASA',
     author_email="Name@statoil.com",
     description="An example data science project",
