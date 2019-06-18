6ebd1275820b52eaf2e0de8ec667d95c0adeb561
mahewitt <mhew@equinor.com>
2019-06-18 12:50:37 +0200

    Added example standalone script

---
+++ {{cookiecutter.repo_name}}/scripts/example.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""An example python script template.
+"""
+
+import sys
+import argparse
+
+
+def main(arguments):
+
+    parser = argparse.ArgumentParser(
+        description="Put your description here",
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('infile', help="Input file", type=argparse.FileType('r'))
+    parser.add_argument('-o', '--outfile', help="Output file",
+                        default=sys.stdout, type=argparse.FileType('w'))
+
+    args = parser.parse_args(arguments)
+
+    print(args)
+
+    # Add your code here
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
